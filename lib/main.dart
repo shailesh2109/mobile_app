c4186d782f48d03eb83518e2a6351fd3aaa3575c
shailesh2109 <74820911+shailesh2109@users.noreply.github.com>
2023-05-30 02:05:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rentico/helpers/color_pallete.dart';
+import 'package:rentico/screens/booking_form.dart';
+import 'package:rentico/screens/home_screen.dart';
 
 import 'screens/splash_screen.dart';
 
